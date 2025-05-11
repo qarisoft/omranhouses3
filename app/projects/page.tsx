7e2dbefb0b6ea8cf298934d4e873163c4e54079e
Salah Al-Salahi <cloud.data.sa@gmail.com>
2025-05-12 02:22:16 +0300

    aa

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardBody, Image } from "@heroui/react";
+// import { Card, CardHeader, CardBody, Image } from "@heroui/react";
 import { Achievements } from "@/components/index/Achements";
 // import { Shareholders } from "@/components/index/Shareholders";
 // import { title } from "@/components/primitives";
@@ -17,23 +17,23 @@ export default function BlogPage() {
 
 
 
-function AppCard() {
-  return (
-    <Card className="py-4">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <p className="text-tiny uppercase font-bold">Daily Mix</p>
-        <small className="text-default-500">12 Tracks</small>
-        <h4 className="font-bold text-large">Frontend Radio</h4>
-      </CardHeader>
-      <CardBody className="overflow-visible py-2">
-        <Image
-          alt="Card background"
-          className="object-cover rounded-xl"
-          src="https://heroui.com/images/hero-card-complete.jpeg"
-          width={270}
-        />
-      </CardBody>
-    </Card>
-  );
-}
+// function AppCard() {
+//   return (
+//     <Card className="py-4">
+//       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+//         <p className="text-tiny uppercase font-bold">Daily Mix</p>
+//         <small className="text-default-500">12 Tracks</small>
+//         <h4 className="font-bold text-large">Frontend Radio</h4>
+//       </CardHeader>
+//       <CardBody className="overflow-visible py-2">
+//         <Image
+//           alt="Card background"
+//           className="object-cover rounded-xl"
+//           src="https://heroui.com/images/hero-card-complete.jpeg"
+//           width={270}
+//         />
+//       </CardBody>
+//     </Card>
+//   );
+// }
 
